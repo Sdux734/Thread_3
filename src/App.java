000d70e807658d94ba4c498ed2424f9076971cb4
davide <duchidavide613@gmail.com>
2025-05-06 10:25:11 +0200

    scheletro

---
+++ src/App.java
@@ -0,0 +1,17 @@
+import java.util.concurrent.Semaphore;
+
+
+public class App {
+    
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    
+    
+    
+    
+    
+    
+    }
+    
+}
+
