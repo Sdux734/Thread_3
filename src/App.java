02816e903b7a270248e8761030280ed5a29cd64b
davide <duchidavide613@gmail.com>
2025-05-06 11:19:00 +0200

    soluzione

--- src/App.java
+++ src/App.java
@@ -2,16 +2,40 @@ import java.util.concurrent.Semaphore;
 
 
 public class App {
-    
+
+    static final int MAX = 4;
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    
-    
-    
-    
-    
-    
+
+        //  main piu elegante
+        String nomi[] = {"marco", "matteo", "tommaso", "giovanni"};
+        Corridore lista[] = new Corridore[MAX];
+
+        for(int i = 0; i < MAX; i++){
+            lista[i] = new Corridore(nomi[i]);
+            lista[i].start();
+        }
+         
+
+        //dichiaro i thread
+
+        Corridore c1 = new Corridore("marco");
+        Corridore c2 = new Corridore("matteo");
+        Corridore c3 = new Corridore("tommaso");
+        Corridore c4 = new Corridore("giovanni");
+
+        //metto in coda (pronti a essere assegnati ad un processo del processore) i thread 
+
+        c1.start();
+        c2.start();
+        c3.start();
+        c4.start();
+
+        }
+     
     }
-    
-}
+        
+        
+
+
 
---
+++ src/Corridore.java
@@ -0,0 +1,35 @@
+import java.util.concurrent.Semaphore;
+
+
+public class Corridore extends Thread{  //estendo la classe Thread
+
+    //dichiaro gli attributi della classe con costruttori getter e setter
+
+    private String nome;
+
+    public Corridore(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    //costruisco il metodo run() dandoli le direttive sul cosa dovranno fare  i thread in esecuzione
+
+    public void run(){
+        for(int i = 0; i <= 100; i++){
+            System.out.println(nome + " metri percorsi: " + i );
+
+            if(i == 100){
+                System.out.println("il corridore " + nome + " e' arrivato");
+            }
+        }
+    }
+
+    
+}
